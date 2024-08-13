74e2e1281ed5f93b2cb35bf506d019f3c1e1bce0
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2024-08-13 21:31:52 +0000

    Se crear el archivo que instalara la dependecias y lanzara el proyecto

---
+++ postCreateCommand.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd /workspaces/frontend-prueba/gestor-tareas
+
+# Instalar dependencias
+echo "Instalando dependencias..."
+# Aquí puedes agregar el comando para instalar dependencias, por ejemplo:
+npm install
+
+# pip install -r requirements.txt
+
+# Lanzar el proyecto
+echo "Lanzando el proyecto..."
+# Aquí puedes agregar el comando para lanzar tu proyecto, por ejemplo:
+npm start
